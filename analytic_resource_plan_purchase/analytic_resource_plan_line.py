a9a2b522b03efbe0b367a288d58534f072635b64
Sudhir Arya <sudhir.arya@serpentcs.com>
2016-03-18 15:43:51 +0530

    [IMP] : migrated analytic_resource_plan_purchase module.

--- analytic_resource_plan_purchase/analytic_resource_plan_line.py
+++ analytic_resource_plan_purchase/analytic_resource_plan_line.py
@@ -1,65 +1,36 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import decimal_precision as dp
-from openerp.osv import fields, osv
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
 
 
-class analytic_resource_plan_line(osv.osv):
-    
+class AnalyticResourcePlanLine(models.Model):
+
     _inherit = 'analytic.resource.plan.line'
 
-    def _has_active_order(self, cr, uid, ids, fields, arg, context=None):
-        res = {}
-        if context is None:
-            context = {}
-
-        for plan_line in self.browse(cr, uid, ids, context=context):
-            res[plan_line.id] = False
+    @api.multi
+    @api.depends('order_line_ids', 'order_line_ids.state')
+    def _has_active_order(self):
+        for plan_line in self:
+            plan_line.has_active_order = False
             for order_line in plan_line.order_line_ids:
                 if order_line.state and order_line.state != 'cancel':
-                    res[plan_line.id] = True
-        return res
+                    plan_line.has_active_order = True
 
-    _columns = {
-        'order_line_ids': fields.many2many('purchase.order.line',
-                                           'analytic_resource_plan_order_line_rel',
-                                           'order_line_id',
-                                           'analytic_resource_plan_line_id'),
+    order_line_ids = fields.Many2many('purchase.order.line',
+                                       'analytic_resource_plan_order_line_rel',
+                                       'order_line_id',
+                                       'analytic_resource_plan_line_id')
 
-        'has_active_order': fields.function(_has_active_order,
-                                            method=True,
-                                            type='boolean',
-                                            string='Order',
-                                            help="Indicates that this resource plan line "
-                                                 "contains at least one non-cancelled purchase order."),
-    }
+    has_active_order = fields.Boolean(compute='_has_active_order', method=True,
+                                      string='Order', help="Indicates that this resource plan line "
+                                      "contains at least one non-cancelled purchase order.")
 
-    def copy(self, cr, uid, id, default=None, context=None):
-        if context is None:
-            context = {}
+    @api.one
+    def copy(self, default=None):
         if default is None:
             default = {}
         default['order_line_ids'] = []
-        res = super(analytic_resource_plan_line, self).copy(
-            cr, uid, id, default, context)
-        return res
-
-analytic_resource_plan_line()
+        return super(AnalyticResourcePlanLine, self).copy(default)
--- analytic_resource_plan_purchase/wizard/analytic_resource_plan_line_make_purchase.py
+++ analytic_resource_plan_purchase/wizard/analytic_resource_plan_line_make_purchase.py
@@ -1,48 +1,30 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#              <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from datetime import datetime
-from openerp.osv import fields, osv, orm
+from openerp import api, fields, models
 from openerp.tools.translate import _
 
 
-class analytic_resource_plan_line_make_purchase(orm.TransientModel):
+class AnalyticResourcePlanLineMakePurchase(models.TransientModel):
     _name = "analytic.resource.plan.line.make.purchase"
     _description = "Resource plan - make purchase"
 
-    def _get_order_lines(self, cr, uid, context=None):
+    @api.model
+    def _get_order_lines(self):
         """
         Returns the order lines associated to the analytic accounts selected.
         """
-        if context is None:
-            context = {}
 
-        record_ids = context and context.get('active_ids', False)
+        record_ids = self._context and self._context.get('active_ids', False)
 
         if record_ids:
             order_line_ids = []
-            line_plan_obj = self.pool.get('analytic.resource.plan.line')
+            line_plan_obj = self.env['analytic.resource.plan.line']
 
-            for line in line_plan_obj.browse(cr, uid, record_ids,
-                                             context=context):
+            for line in line_plan_obj.browse(record_ids):
                     for order_line in line.order_line_ids:
                         order_line_id = order_line and order_line.id
                         order_line_ids.extend([order_line_id])
@@ -50,18 +32,13 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
                 return order_line_ids
         return False
 
-    _columns = {
-        'order_line_ids': fields.many2many('purchase.order.line',
-                                           'make_purchase_order_line_rel',
-                                           'order_line_id',
-                                           'make_purchase_order_id'),
-    }
+    order_line_ids = fields.Many2many('purchase.order.line',
+                                      'make_purchase_order_line_rel',
+                                      'order_line_id',
+                                      'make_purchase_order_id', default=_get_order_lines)
 
-    _defaults = {
-        'order_line_ids': _get_order_lines,
-    }
-
-    def make_purchase_orders(self, cr, uid, ids, context=None):        
+    @api.multi
+    def make_purchase_orders(self):
         """
              To make purchases.
 
@@ -75,49 +52,42 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
 
         """
 
-        if context is None:
-            context = {}
-        record_ids = context and context.get('active_ids', False)
+        record_ids = self._context and self._context.get('active_ids', False)
         res = []
-        if record_ids:            
-            line_plan_obj = self.pool.get('analytic.resource.plan.line')
-            order_obj = self.pool.get('purchase.order')
-            order_line_obj = self.pool.get('purchase.order.line')
-            partner_obj = self.pool.get('res.partner')                                            
-            acc_pos_obj = self.pool.get('account.fiscal.position')           
-            
+        if record_ids:
+            line_plan_obj = self.env['analytic.resource.plan.line']
+            order_obj = self.env['purchase.order']
+            order_line_obj = self.env['purchase.order.line']
+            partner_obj = self.env['res.partner']
+            acc_pos_obj = self.env['account.fiscal.position']
+
             location_ids = []
             list_line = []
             supplier_data = False
             company_id = False
             purchase_id = False
 
-            for line in line_plan_obj.browse(cr, uid, record_ids,
-                                             context=context):
+            for line in line_plan_obj.browse(record_ids):
                 if not line.supplier_id:
-                    raise osv.except_osv(
-                        _('Could not create purchase order !'),
+                    raise Warning(_('Could not create purchase order !'),
                         _('You have to enter a supplier.'))
 
                 if supplier_data is not False \
                         and line.supplier_id.id != supplier_data:
-                    raise osv.except_osv(
-                        _('Could not create purchase order !'),
+                    raise Warning(_('Could not create purchase order !'),
                         _('You have to select lines '
                           'from the same supplier.'))
                 else:
                     supplier_data = line.supplier_id.id
 
-                address_id = partner_obj.address_get(
-                    cr, uid, [line.supplier_id.id],
+                address_id = partner_obj.address_get([line.supplier_id.id],
                     ['delivery'])['delivery']
                 partner = line.supplier_id
                 line_company_id = line.company_id \
                     and line.company_id.id or False
                 if company_id is not False \
                         and line_company_id != company_id:
-                    raise osv.except_osv(
-                        _('Could not create purchase order !'),
+                    raise Warning(_('Could not create purchase order !'),
                         _('You have to select lines '
                           'from the same company.'))
                 else:
@@ -128,12 +98,9 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
 
                 account_id = line_account_id
 
-                warehouse_obj = self.pool.get('stock.warehouse')
-                warehouse_ids = warehouse_obj.search(
-                    cr, uid, [('company_id', '=', company_id)])
-                if warehouse_ids:
-                    warehouses = warehouse_obj.browse(
-                        cr, uid, warehouse_ids, context=context)
+                warehouse_obj = self.env['stock.warehouse']
+                warehouses = warehouse_obj.search([('company_id', '=', company_id)])
+                if warehouses:
                     location_ids = []
                     for lot_stock_ids in warehouses:
                         location_ids.append(lot_stock_ids.lot_stock_id.id)
@@ -156,8 +123,7 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
                 if line.product_id:
                     taxes_ids = \
                         line.product_id.product_tmpl_id.supplier_taxes_id
-                    taxes = acc_pos_obj.map_tax(
-                        cr, uid, partner.property_account_position, taxes_ids)
+                    taxes = partner.property_account_position.map_tax(taxes_ids)
 
                 if taxes:
                     purchase_order_line.update({
@@ -166,8 +132,7 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
                 list_line.append(purchase_order_line)
 
                 if purchase_id is False:
-                    purchase_id = order_obj.create(
-                        cr, uid,
+                    purchase = order_obj.create(
                         {'origin': '',
                          'partner_id': line.supplier_id.id,
                          'partner_address_id': address_id,
@@ -182,24 +147,20 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
                             partner.property_supplier_payment_term
                             and partner.property_supplier_payment_term.id
                             or False
-                         }, context=context)
+                         })
                     if line.account_id.user_id:
-                        order_obj.message_subscribe_users(
-                            cr, uid, [purchase_id],
-                            user_ids=[line.account_id.user_id.id])
+                        purchase.message_subscribe_users(user_ids=[line.account_id.user_id.id])
 
                 purchase_order_line.update({
-                    'order_id': purchase_id
+                    'order_id': purchase.id
                 })
 
-                order_line_id = order_line_obj.create(
-                    cr, uid, purchase_order_line, context=context)
+                order_line = order_line_obj.create(purchase_order_line)
                 values = {
-                    'order_line_ids': [(4, order_line_id)]
+                    'order_line_ids': [(4, order_line.id)]
                 }
-                line_plan_obj.write(cr, uid, [line.id], values,
-                                    context=context)
-                res.append(order_line_id)
+                line.write(values)
+                res.append(order_line.id)
 
         return {
             'domain': "[('id','in', ["+','.join(map(str, res))+"])]",
@@ -211,5 +172,3 @@ class analytic_resource_plan_line_make_purchase(orm.TransientModel):
             'context': False,
             'type': 'ir.actions.act_window'
         }
-
-analytic_resource_plan_line_make_purchase()
